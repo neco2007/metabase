9bcc9a26d36c75500f0bf4ea03be7843d54cc977
neco2007 <daiching1104@gmail.com>
2025-12-29 12:18:17 +0900

    a

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import VCComponent from './VCComponent'
 
 // テスト用：実際は認証システムから取得したトークンを渡します
-const testToken = "YOUR_DEBUG_TOKEN_HERE"; 
+const testToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8wMDEiLCJleHAiOjE3NjcwOTQ2NDd9.FPyV7gMyaeyzH3lx60V_FlMUiLuxYMeosu7euIc2Xok"; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
