cdd1e2ceeec2b63354b592b2c5a2f9038da03ed5
neco2007 <daiching1104@gmail.com>
2025-12-29 11:31:47 +0900

    aa

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ async def signaling_endpoint(offer: dict, authorization: Optional[str] = Header(
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8099, reload=True)
